1a98b5622442fc6746793d58a84a0fde523b601f
namlin <driscollucs@gmail.com>
2026-09-06 03:55:29 -0600

    Implementation of the update, add_entity_to_systems and remove_entity_from systems for the class Registry.

--- src/Registry.cc
+++ src/Registry.cc
@@ -34,4 +34,45 @@ void Registry::delete_entity(Entity entity) {
   this->entities_to_remove.insert(entity);
 }
 
+void Registry::add_entity_to_systems(Entity entity) {
+  const auto id = entity.get_id();
+  const std::bitset<MAX_COMPONENTS>& entity_component_signature = this->entity_component_signatures[id];
+
+  for (auto& system_pair : this->systems) {
+    const auto& system = system_pair.second;
+    const auto& system_component_signature = system->get_component_signature();
+
+    bool match = (entity_component_signature & system_component_signature) == system_component_signature;
+
+    if (match) {
+      system->add_entity(entity);
+    }
+  }
+}
+
+void Registry::remove_entity_from_systems(Entity entity) {
+  for (auto& system_pair : this->systems) {
+    system_pair.second->remove_entity(entity);
+  }
+}
+
+void Registry::update(void) {
+  for (auto entity : this->entities_to_add) {
+    this->add_entity_to_systems(entity);
+  }
+
+  this->entities_to_add.clear();
+
+  for (auto entity : this->entities_to_remove) {
+    this->remove_entity_from_systems(entity);
+
+    // Set the whole bitset to 0:
+    this->entity_component_signatures[entity.get_id()].reset();
+
+    // TODO: add an ID to the deque of free ID's.
+  }
+
+  this->entities_to_remove.clear();
+}
+
 // void Registry::add_system(Entity) {}
