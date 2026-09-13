dfb12e30397db159597c387492303f81130cb0cb
namlin <driscollucs@gmail.com>
2026-09-13 17:11:42 -0600

    Implementation of the functions subscribe_to_event, emit_event and reset for the class EventManager.

--- include/EventManager.h
+++ include/EventManager.h
@@ -1,6 +1,12 @@
 #ifndef EVENTMANAGER_H
 #define EVENTMANAGER_H
 
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <typedefindex>
+
 // Interface.
 class IEventCallback {
  private:
@@ -36,6 +42,49 @@ class EventCallback : public IEventCallback {
   EventCallback(TOwner* owner_instance, CallbackFunction callback_fucntion) {
     this->owner_instance = owner_instance;
     this->callback_function = callback_function;
+  }
+};
+
+typedef std::list<IEventCallback*> HandlerList;
+
+class EventManager {
+ private:
+  std::map<std::type_index, handler_list*> subscribers;
+
+ public:
+  EventManager(void) {
+    std::cout << "[EventManager] Constructor Executing.\n";
+  }
+
+  ~EventManager(void) {
+    std::cout << "[EventManager] Destructor Executing.\n";
+  }
+
+  void reset(void) {
+    this->subscribers.clear();
+  }
+
+  template <typename TEvent, typename TOwner>
+  void subscribe_to_event(TOwner* owner_instance, void (TOwner::*callback_function)(TEvent&)) {
+    if (!this->subscribers[typeid(TEvent)].get()) {
+      this->subscribers[typeid(TEvent)] = new HandlerList();
+    }
+
+    auto subscriber = new EventCallback<TOwner, TEvent>(owner_instance, callback_function);
+    this->subscribers[typeid(TEvent)]->push_back(std::move(subsciber));
+  }
+
+  template <typename TEvent, typename ... TArgs>
+  void emit_event(TArgs&% ... args) {
+    auto handlers = this->subscribers[typeid(TEvent)].get();
+
+    if (handlers) {
+      for (auto it = handlers->begin(); it != handlers.end(); it++) {
+        auto handler = it->get();
+        TEvent event(std::forward<TArgs>(args)...);
+        hanlder->execute(event);
+      }
+    }
   }
 };
 
